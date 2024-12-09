4cfde7fda96d95b0451d07f72dba1ea961456fe2
Ali Sabouni <alisabouni@stu.aydin.edu.tr>
2024-12-09 19:53:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -82,5 +82,11 @@ app.get('/categories', (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// const PORT = 3000;
+// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
